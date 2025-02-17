4d6212d49306dcb586d0235f437c8855b1c26bcf
EdoardoGiancarli <edo.giancarli@gmail.com>
2025-02-17 18:22:17 +0100

    edits and methods for BM

--- Img_Reconstruction_RealMasks/_insert_into_bm.py
+++ Img_Reconstruction_RealMasks/_insert_into_bm.py
@@ -1,8 +1,7 @@
-"""
-Module for testing IROS reconstruction.
-"""
-
+from collections.abc import Sequence
 from copy import deepcopy
+from pathlib import Path
+
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
@@ -10,43 +9,9 @@ from astropy.io import fits
 from tqdm import tqdm
 import pickle
 
-from mbloodmoon.images import argmax, compose, upscale
+from mbloodmoon.images import argmax
 from mbloodmoon.coords import shift2equatorial
 import mbloodmoon as bm
-
-matplotlib.use('agg')
-root_path = "/mnt/d/PhD_AASS/Coding/Images_fits/"
-
-def plot_cameras(skyrecs, name) -> None:
-    sky_a, sky_b = skyrecs
-    fig, axs = plt.subplots(1, 2, figsize=(12, 6), dpi=150)
-    plt.tight_layout()
-    for ax, b, bmax, title in zip(
-            axs,
-            [sky_a, sky_b],
-            [argmax(sky_a), argmax(sky_b)],
-            ["SkyRec CamA", "SkyRec CamB"],
-    ):
-        ax.imshow(b, vmin=0, vmax=-b.min())
-        ax.scatter(bmax[1], bmax[0], facecolors='none', edgecolors='white', alpha=0.5)
-        ax.set_title(title, fontsize=14, pad=8, fontweight='bold')
-    plt.savefig(root_path + name + '.png')
-    plt.close()
-
-def plot_skyrec(skyrecs, title, source_indices=None, source_names=None, dpi=200, upsc_y=8):
-    composed, _ = compose(*skyrecs, strict=False)
-    fig, ax = plt.subplots(1, 1, figsize=(8, 10), dpi=dpi)
-    if source_indices is not None and source_names is not None:
-        for ((i, j), name) in zip(source_indices, source_names):
-            ax.scatter(j, i * upsc_y + 53, s=30, facecolors="none", edgecolors="white", alpha=1., linewidth=.5)
-            ax.text(j + 50 , i * upsc_y + 100, name, color="white", fontsize=4)
-    im = ax.imshow(composed, vmax=np.quantile(composed, 0.9995), vmin=0., cmap="viridis")
-    plt.colorbar(im, ax=ax, label='SNR', fraction=0.025, aspect=35, pad=0.02, shrink=0.33, location="bottom")
-    ax.set_title(title, fontsize=12, pad=8, fontweight='bold')
-    plt.axis("off")
-    plt.tight_layout()
-    plt.savefig(root_path + title.replace(' ', '_').lower() + ".png")
-    plt.close()
 
 
 def perform_IROS(simul_data: str,
@@ -154,65 +119,97 @@ def perform_IROS(simul_data: str,
 
 
 
+def save_iros_output(data: dict,
+                     mask_file: str | Path,
+                     save_to: str | Path,
+                     ) -> None:
+    """
+    Saves the IROS output data.
+    """
+    print("# Saving data...")
+
+    def make_column(name: str,
+                    col_data: Sequence,
+                    data_format: str,
+                    unit: str,
+                    ) -> fits.Column:
+        return fits.Column(name=f"{name.upper()}", array=col_data,
+                           format=data_format, unit=unit)
+
+    def make_bintable(name: str,
+                      tab_data: list,
+                      sdl_header: fits.Header,
+                      ) -> fits.BinTableHDU:
+        table_hdu = fits.BinTableHDU.from_columns(
+            columns=tab_data,
+            header=sdl_header,
+            name=f"{name.upper()}",
+        )
+        return table_hdu
+
+    # HDU list and Primary Header
+    hdu_list = fits.HDUList([])
+    primary_header = fits.getheader(mask_file, ext=2)
+    primary_header['EXTNAME'] = 'PRIMARY'
+    primary_hdu = fits.PrimaryHDU(header=primary_header)
+    hdu_list.append(primary_hdu)
+
+    # BinTables
+    for camera in data.keys():
+        cam = data[camera]
+        columns = [
+            make_column(key, cam[key]["data"], cam[key]["format"], cam[key]["unit"])
+            for key in list(cam.keys())[:-1]
+        ]
+        table_hdu = make_bintable(camera, columns, cam["info"])
+        hdu_list.append(table_hdu)
+    
+    # save data
+    hdu_list.writeto(save_to)
+    print("# Saving completed!")
 
 
-if __name__ == '__main__':
 
-    ## paths for mask and data .fits
-    mask_file = root_path + "wfm_mask.fits"
-    simul_data = root_path + "iros_simulation_GC_LMC/20241011_galctr_rxte_sax_2-30keV_1ks_2cams_sources_cxb/"  #lmc_rxte_sax_2-30keV_10ks_sources_cxb/
-    # galctr_rxte_sax_2-30keV_1ks_sources_cxb/
+def save_pickle(data: object, save_to: str | Path) -> None:
+    """
+    Saves data in .pickle format.
 
-    ## select data
-    cam_a = "cam1a"
-    cam_b = "cam1b"
-    dataset = "reconstructed"
-    # filepaths = bm.simulation_files(simul_data)
-    # wfm = bm.codedmask(mask_file, upscale_x=5)
-    # sdl_1a = bm.simulation(filepaths["cam1a"][dataset])
-    # sdl_1b = bm.simulation(filepaths["cam1b"][dataset])
+    Args:
+        - data: object
+        Data to save.
+        - save_to: str | Path
+        Path to save the data
+    """
+    print("# Saving data...")
+    with open(save_to + ".pickle", "wb") as handle:
+        pickle.dump(data, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    print("# Saving completed!")
 
-    ## save data and catalog comparison
-    save_file = False
-    compare_w_catalog = False
 
-    save_to = root_path + "iros_performance_test.fits" if save_file else None
-    catalog = bm.simulation_files(simul_data)[cam_a]['sources'] if compare_w_catalog else None    # TODO
 
-    ## perform IROS
-    max_iterations = 5
-    snr_threshold = 5
+def load_iros_output(filepath: str | Path) -> dict:
+    print("# Loading data...")
 
-    iros_log = perform_IROS(
-        simul_data=simul_data,
-        mask_file=mask_file,
-        cam_a=cam_a,
-        cam_b=cam_b,
-        dataset=dataset,
-        max_iterations=max_iterations,
-        snr_threshold=snr_threshold,
-    )
+    print("# Loading completed!")
 
-    ## comparing IROS reconstruction with catalog
-    #source_indices, source_names = compare_w_catalog(iros_log, catalog)
 
-    ## saving IROS reconstruction
-    ...
+def load_pickle(filepath: str | Path) -> object:
+    """
+    Loads data from .pickle file.
+    
+    Args:
+        - filepath: str | Path
+    """
+    print("# Loading data...")
+    with open(filepath + ".pickle", "rb") as handle:
+        data = pickle.load(handle)
+    print("# Loading completed!")
+    return data
 
-    ## plot
-    # upscaled_cams = [upscale(cam, upscale_y=8) for cam in ...]
-    # upscaled_snrs = [upscale(snr, upscale_y=8) for snr in ...]
- 
-    # print("Trying to plot damn cameras big images...")
-    # plot_cameras(upscaled_cams, "cams")
-    # print("Cameras done, now the SNRs...")
-    # plot_cameras(upscaled_snrs, "snrs_cams")
-    # print("Trying to plot damn composed big images...")
-    # plot_skyrec(upscaled_cams, "Galactic Center IROS rec")
- 
-    # print("Sky done, now the SNR...")
-    # title = "SNR CAM composition"
-    # plot_skyrec(upscaled_snrs, source_indices, source_names, title)
+
+
+def compare_w_catalog() -> dict:
+    pass
 
 
 # end
