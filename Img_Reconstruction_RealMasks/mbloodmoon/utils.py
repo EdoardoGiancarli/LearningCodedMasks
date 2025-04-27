d7ec8918dec7f2672847fa2aff6eceedb2815979
EdoardoGiancarli <edo.giancarli@gmail.com>
2025-04-27 22:51:00 +0200

    Edit timer

--- Img_Reconstruction_RealMasks/mbloodmoon/utils.py
+++ Img_Reconstruction_RealMasks/mbloodmoon/utils.py
@@ -13,31 +13,25 @@ __all__ = [
 
 
 @contextmanager
-def timer(
-    name: str,
-    units: str = "s",
-) -> Generator[None, Any, None]:
+def timer(name: str) -> Generator[None, Any, None]:
     """
     Timer context manager.
 
     Args:
-        name (str):
-            Name of the task the timer is handling.
-        units (str, optional (default='s')):
-            Units of the measured time (seconds 's', minutes 'min' or hours 'h').
+        name (str): Name of the task the timer is handling.
     
     Raises:
-        ValueError: For invalid 'units' input.
         Exception: Encoutered exception within the algorithm (if any).
-    
-    TODO: insert units for final elapsed time.
     """
-    if units not in ["s", "min", "h"]:
-        raise ValueError(f"Invalid 'units' {units}, choose between 's', 'min' or 'h'.")
+    def handle_time(start: float, end: float) -> str:
+        """Handles output time format."""
+        time_interval = end - start
+        h, rem = divmod(time_interval, 3600)
+        m, s = divmod(rem, 60)
+        return f"{int(h):02}h:{int(m):02}m:{s:.3f}s"
 
     start_time = time.perf_counter()
     error = False
-
     print(f"    # Starting '{name}' at {datetime.now():%H:%M:%S}.")
     try:
         yield
@@ -46,7 +40,8 @@ def timer(
         raise
     finally:
         end_time = time.perf_counter()
-        mess = f"    # Finished '{name}' in {end_time - start_time:.3f}s"
+        elapsed_time = handle_time(start_time, end_time)
+        mess = f"    # Finished '{name}' in {elapsed_time}"
         print(mess + ".\n" if not error else mess + f" with {error}.\n")
 
 
