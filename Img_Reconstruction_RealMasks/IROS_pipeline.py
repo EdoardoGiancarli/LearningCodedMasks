772f633d3c63194320394e3cb168a1c6a6144cf3
EdoardoGiancarli <edo.giancarli@gmail.com>
2025-05-05 17:40:40 +0200

    Tests on residues

--- Img_Reconstruction_RealMasks/IROS_pipeline.py
+++ Img_Reconstruction_RealMasks/IROS_pipeline.py
@@ -150,12 +150,12 @@ if __name__ == "__main__":
     mask_FITS = "wfm_mask.fits"
     IDEAL_MASK = False                 # infinitely opaque and thin mask
 
-    N_TEST = "pivot_fine_model_vignetting2"
-    UPSX_0, UPSY_0 = 4, 1              # initial upscaling (with which IROS is performed)
-    UPSX_FINAL, UPSY_FINAL = 4, 1      # final upscaling for skies and visualisation
+    N_TEST = "thinmask_realdet_distances_offset_detected"
+    UPSX_0, UPSY_0 = 5, 1              # initial upscaling (with which IROS is performed)
+    UPSX_FINAL, UPSY_FINAL = 5, 1      # final upscaling for skies and visualisation
 
     skyfield = "GalacticCenter"
-    data_FITS = "20241011_galctr_rxte_sax_2-30keV_1ks_2cams_sources_cxb"
+    data_FITS = "20250430_galctr_rxte_sax_2-50keV_1ks_thinmask_realdet_sources_cxb"
     
     mask_file, simul_data, save_path = _handle_dirpaths(
         mask=mask_FITS,
--- Img_Reconstruction_RealMasks/mbloodmoon/io.py
+++ Img_Reconstruction_RealMasks/mbloodmoon/io.py
@@ -215,19 +215,19 @@ class MaskDataLoader:
                 - "detector_maxx": bottom physical detector edge along y-axis [mm]
                 - "detector_miny": right physical detector edge along x-axis [mm]
                 - "detector_maxy": top physical detector edge along y-axis [mm]
-                - "detector_bmmask_dist": detector - bottom mask distance [mm] (with detector median absorption)
-                - "detector_midmask_dist": detector - mid mask plate distance [mm] (with detector median absorption)
-                - "detector_topmask_dist": detector - top mask distance [mm] (with detector median absorption)
+                - "detector_bmmask_dist": detector - bottom mask distance [mm] (with detector median absorption distance)
+                - "detector_midmask_dist": detector - mid mask plate distance [mm] (with detector median absorption distance)
+                - "detector_topmask_dist": detector - top mask distance [mm] (with detector median absorption distance)
                 - "open_fraction": mask open fraction
                 - "real_open_fraction": mask open fraction with ribs correction
         
         Notes:
-            - The detector median absorption for the incident photons is set to 0.01 mm.
+            - The detector median absorption distance for the incident photons is set to 0.01 mm.
         """
         h1 = dict(fits.getheader(self.filepath, ext=0)) | dict(fits.getheader(self.filepath, ext=2))
         h2 = dict(fits.getheader(self.filepath, ext=3))
 
-        detector_absorption = 0.01     # median photon length absorption in the detector [mm]
+        detector_absrp_dist = 0.01     # median photon absorption distance in the detector [mm]
 
         info = {
             "mask_minx": h1["MINX"],
@@ -243,9 +243,9 @@ class MaskDataLoader:
             "detector_maxx": h1["PLNXMAX"],
             "detector_miny": h1["PLNYMIN"],
             "detector_maxy": h1["PLNYMAX"],
-            "detector_bmmask_dist": h1["MDDIST"] + detector_absorption,
-            "detector_midmask_dist": h1["MDDIST"] + h1["MASKTHK"] / 2 + detector_absorption,
-            "detector_topmask_dist": h1["MDDIST"] + h1["MASKTHK"] + detector_absorption,
+            "detector_bmmask_dist": h1["MDDIST"] + detector_absrp_dist,
+            "detector_midmask_dist": h1["MDDIST"] + h1["MASKTHK"] / 2 + detector_absrp_dist,
+            "detector_topmask_dist": h1["MDDIST"] + h1["MASKTHK"] + detector_absrp_dist,
             "open_fraction": h2["OPENFR"],
             "real_open_fraction": h2["RLOPENFR"]
         }
