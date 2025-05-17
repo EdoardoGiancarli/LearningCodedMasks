f10a6dfb6275987f8c229344adf784a2a1e40eb8
EdoardoGiancarli <edo.giancarli@gmail.com>
2025-05-17 18:17:21 +0200

    Filters update

--- Img_Reconstruction_RealMasks/mbloodmoon/iros_management/filtering.py
+++ Img_Reconstruction_RealMasks/mbloodmoon/iros_management/filtering.py
@@ -10,19 +10,19 @@ import numpy as np
 
 def data_filter(
     record: np.recarray,
-    energy_range: int | float | tuple[int | float] | None,
-    coords: tuple[float] | Sequence[tuple[float]] | None,
+    energy_range: int | float | tuple[int | float, int | float] | None,
+    coords: tuple[float, float] | Sequence[tuple[float, float]] | None,
 ) -> np.recarray:
     """
     Filters the input `record` based on the photons energy and/or position.
     
     Args:
         record (np.recarray): Input simulated data container.
-        energy_range (int | float | tuple[int | float] | None):
+        energy_range (int | float | tuple[int | float, int | float] | None):
             Energy range in keV for the data filtering. If a specific energy
             is given, this will be considered as the maximum filter value.
             If a tuple is given, it's interpreted as (`E_min`, `E_max`).
-        coords (tuple[float] | Sequence[tuple[float]] | None):
+        coords (tuple[float, float] | Sequence[tuple[float, float]] | None):
             Input photons RA/Dec (or sequence of RA/Dec) to filter out.
     
     Returns:
@@ -30,7 +30,7 @@ def data_filter(
     """
     def _energy_mask(
         mask: npt.NDArray,
-        values: int | float | tuple[int | float],
+        values: int | float | tuple[int | float, int | float],
     ) -> npt.NDArray:
         """Creates an energy mask for the input `record`."""
         if isinstance(values, (int, float)):
@@ -41,7 +41,7 @@ def data_filter(
 
     def _coords_mask(
         mask: npt.NDArray,
-        values: tuple[float],
+        values: tuple[float, float],
     ) -> npt.NDArray:
         """Creates a RA/Dec mask for the input `record`."""
         # to address float64 to float32 conv, we remove
@@ -71,13 +71,13 @@ def data_filter(
 
 
 def flux_filter(
-    flux_range: int | float | tuple[int | float],
+    flux_range: int | float | tuple[int | float, int | float],
 ) -> Callable[[np.recarray], np.recarray]:
     """
-    Filters the input `record` for a given flux range.
+    Filters the input catalog `record` for a given flux range.
 
     Args:
-        flux_range (int | float | tuple[int | float]):
+        flux_range (int | float | tuple[int | float, int | float]):
             Flux range in ph/cm2/s for the data filtering. If a specific flux
             is given, this will be considered as the minimum filter value.
             If a tuple is given, it's interpreted as (`F_min`, `F_max`).
@@ -108,15 +108,15 @@ def flux_filter(
     return apply
 
 
-def source_filter(n: int | tuple[int]) -> Callable[[np.recarray], np.recarray]:
+def source_filter(n: int | tuple[int, int]) -> Callable[[np.recarray], np.recarray]:
     """
-    Select the `n` brightest sources from the input `record`,
+    Select the `n` brightest sources from the input catalog `record`,
     or a given interval of sources.
 
     Args:
         n (int | tuple[int]):
             Filtered interval of sources, up to the n-th brightest
-            source or from n[0] to n[1] if `n` is a tuple.
+            source or from `n[0]` to `n[1]` if `n` is a tuple.
 
     Returns:
         apply (Callable[[np.recarray], np.recarray]):
@@ -124,7 +124,7 @@ def source_filter(n: int | tuple[int]) -> Callable[[np.recarray], np.recarray]:
     """
     def apply(record: np.recarray) -> np.recarray:
         """
-        Applies the filter in the specified flux range.
+        Applies the filter for the specified number of sources.
 
         Args:
             record (np.recarray): Input simulated data container.
@@ -132,11 +132,10 @@ def source_filter(n: int | tuple[int]) -> Callable[[np.recarray], np.recarray]:
         Returns:
             output (np.recarray): Output filtered data container.
         """
-        sorted_record = np.sort(record, order="NPHOTONS")
-        return sorted_record[-n:] if isinstance(n, int) else sorted_record[-n[1] : -n[0]]  #!!!TODO: configure sources idx in sorted rec
+        sorted_record = np.sort(record, order="NPHOTONS")[::-1]
+        return sorted_record[:n] if isinstance(n, int) else sorted_record[n[0] : n[1]]
     
     return apply
-
 
 
 # end
