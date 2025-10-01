c4133fa3520c52d694666538a3e422f013b824ac
EdoardoGiancarli <edo.giancarli@gmail.com>
2025-10-01 11:25:32 +0200

    camera angular resolution

--- Darksun_Reconstruction/singleCAM_IROS/singleCAM_iros.py
+++ Darksun_Reconstruction/singleCAM_IROS/singleCAM_iros.py
@@ -154,7 +154,7 @@ def camera_angular_resolution(camera: CodedMaskCamera) -> tuple[float, float]:
         * From: Skinner, G.K., 2008. Sensitivity of coded mask telescopes.
           Applied optics, 47(15), pp.2739-2749.
     """
-    def angular_resolution(m: float, d: float, p: float) -> float:
+    def angular_resolution(m_pitch: float, d_pitch: float, dist: float) -> float:
         """
         Computes the camera angular resolution along the axis, in [arcmin].
 
@@ -163,14 +163,16 @@ def camera_angular_resolution(camera: CodedMaskCamera) -> tuple[float, float]:
             d (float): Detector element resolution pitch.
             p (float): Mask - Detector distance.
         """
-        dtheta = np.sqrt(np.square(m / p) + np.square(d / p))  # [rad]
-        dtheta_arcmin = np.rad2deg(dtheta) * 60
+        dtheta_rad = np.sqrt(
+            np.square(m_pitch / dist) + np.square(d_pitch / dist)
+        )
+        dtheta_arcmin = np.rad2deg(dtheta_rad) * 60
         return dtheta_arcmin
     
     p = camera.specs['mask_detector_distance']
     mx, my = (
-        camera.specs[''],
-        camera.specs[''],
+        camera.specs['slit_deltax'],
+        camera.specs['slit_deltay'],
     )
     dx, dy = (
         camera.specs[''],
@@ -184,8 +186,11 @@ def camera_angular_resolution(camera: CodedMaskCamera) -> tuple[float, float]:
 
 def camera_skycoords_errors(camera: CodedMaskCamera) -> tuple[float, float]:
     """
-    Computes the camera local-frame coords errors along the
-    axes taking into account the chosen camera upscaling.
+    Computes the camera local-frame coords NOMINAL* errors along
+    the axes, taking into account the chosen camera upscaling.
+
+    *For now, we are considering a proxy for the camera angular
+     resolution along the fine and coarse directions.
 
     Args:
         camera (CodedMaskCamera):
@@ -202,28 +207,35 @@ def camera_skycoords_errors(camera: CodedMaskCamera) -> tuple[float, float]:
     
     UPX, UPY = camera.upscale_f
     ang_res_x, ang_res_y = camera_angular_resolution(camera)
-    dsx = abs(angle2shift(camera, arcmin2deg(ang_res_x))) / UPX  # [mm]
-    dsy = abs(angle2shift(camera, arcmin2deg(ang_res_y))) / UPY  # [mm]
+    dsx = abs(angle2shift(camera, arcmin2deg(ang_res_x / UPX)))  # [mm]
+    dsy = abs(angle2shift(camera, arcmin2deg(ang_res_y / UPY)))  # [mm]
     return dsx, dsy
 
 
 def shifts_errors(camera: CodedMaskCamera) -> tuple[float, float]:
     """
-    Computes the camera local-frame coords sensitivity along the axes.
+    Computes the camera local-frame coords NOMINAL* errors along
+    the axes, taking into account the chosen camera upscaling.
 
-    TODO:
-        * insert correct camera sensitivity estimation (this is a proxy,
-          dthetax = 5 arcmin, dthetay = 60 arcmin at (upx, upy) = (1, 1)).
+    *For now, we are considering a proxy for the camera angular
+     resolution along the fine and coarse directions.
+    
+    At upscaling (upx, upy) = (1, 1) we select:
+        - dthetax = 5 arcmin along the fine direction
+        - dthetay = 60 arcmin along the coarse direction
     """
     def arcmin2deg(angle: float) -> float:
         """Converts angle from [arcmin] to [deg]."""
         return angle / 60
     
     UPX, UPY = camera.upscale_f
-    dthetax = 5.0 / UPX                                  # [arcmin]
-    dthetay = 60.0 / UPY                                 # [arcmin]
-    dsx = abs(angle2shift(camera, arcmin2deg(dthetax)))  # [mm]
-    dsy = abs(angle2shift(camera, arcmin2deg(dthetay)))  # [mm]
+    # camera angular resolution at given upscaling [arcmin]
+    dthetax, dthetay = 5.0 / UPX, 60.0 / UPY
+    # local-frame sky-shifts errors [mm]
+    dsx, dsy = map(
+        lambda x: abs(angle2shift(camera, arcmin2deg(x))),
+        (dthetax, dthetay),
+    )
     return dsx, dsy
 
 
