09edfb8458163f1d9f3751b2e01ad8f3fa5d963c
EdoardoGiancarli <edo.giancarli@gmail.com>
2025-11-14 15:50:25 +0100

    update catalogue comparison

--- Darksun_Reconstruction/tests/assets/__init__.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-
-_path_test_mask = Path(__file__).parent / "wfm_mask.fits"
-_path_test_SDL = Path(__file__).parent / "SDL.fits"
-_path_test_catalogue = Path(__file__).parent / "catalogue.fits"
-
-
-# end
