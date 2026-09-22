618da009dd60dc95f9897acc712e742e57b87c42
EdoardoGiancarli <edo.giancarli@gmail.com>
2026-09-22 19:11:25 +0200

    [dev] analysing fit performance and PSLA for iros paper

--- IROS/run_IROS.sh
+++ IROS/run_IROS.sh
@@ -1,21 +1,29 @@
 echo "Running IROS reconstruction pipeline for Unit-1 of the LEM-X Observatory..."
 echo ""
 
-python IROSrec_pipeline__argparsed.py --help
+uv run IROSrec_pipeline__argparsed.py --help
 
-python IROSrec_pipeline__argparsed.py \
-   --run GC_smoothed_rec_1ks_upx5_mask25 \
-   --skyfield GalacticCentre \
-   --datadir galctr_rxte-sax_mask_050_1040x17_2-50keV_1ks_mask25 \
+uv run IROSrec_pipeline__argparsed.py \
+   --run IROSbncmrk_detected_1ks_upx2_infdense_NewSGNorm_mask25 \
+   --skyfield IROSDummy \
+   --datadir iros_benchmark_2-50keV_mask_050_1040x17_infdet_1ks_mask25 \
+   --dataset detected \
    --mask_pattern mask_NTHT_20250725.fits \
-   --up_fine 5 \
-   --smoothing \
-   --smoothing_snr_thresh 15.0 \
-   --baseline_irosrec /mnt/dbb8f47e-da06-47bf-8ef5-038092af70f7/Edos_Magnificent_Manor/PhD_AASS/Coding/IROS_Data/Outputs/OutGalacticCentre/galctr_rxte-sax_mask_050_1040x17_2-50keV_1ks_mask25/GC_rec_1ks_upx5_mask25/ \
-   # --energy_range 2.0 6.0 \
-   # --compose_unit \
+   # --up_fine 5 \
 
-# python IROSrec_pipeline__argparsed.py \
+# uv run IROSrec_pipeline__argparsed.py \
+#    --run GC_smoothed_rec_1ks_upx5_mask25 \
+#    --skyfield GalacticCentre \
+#    --datadir galctr_rxte-sax_mask_050_1040x17_2-50keV_1ks_mask25 \
+#    --mask_pattern mask_NTHT_20250725.fits \
+#    --up_fine 5 \
+#    --smoothing \
+#    --smoothing_snr_thresh 15.0 \
+#    --baseline_irosrec /mnt/dbb8f47e-da06-47bf-8ef5-038092af70f7/Edos_Magnificent_Manor/PhD_AASS/Coding/IROS_Data/Outputs/OutGalacticCentre/galctr_rxte-sax_mask_050_1040x17_2-50keV_1ks_mask25/GC_rec_1ks_upx5_mask25/ \
+#    # --energy_range 2.0 6.0 \
+#    # --compose_unit \
+
+# uv run IROSrec_pipeline__argparsed.py \
 #    --run GC_rec_1ks_detected_2-6keV_smoothed_mask25 \
 #    --skyfield GalacticCentre \
 #    --datadir galctr_rxte-sax_mask_050_1040x17_2-50keV_1ks_mask25 \
